5342de37826b4a98e3962ff994c32210f316f00b
CPSDC <CPSDC@DESKTOP-7T1PPNB>
2018-01-19 16:17:46 +0800

    dd

--- spring4study/DB/scheam.sql
+++ spring4study/DB/scheam.sql
@@ -3,4 +3,19 @@ CREATE TABLE `hh2`.`test_user` (
   `name` VARCHAR(64) NULL,
   PRIMARY KEY (`id`));
   
-  
+create table USERS
+(
+  ID                INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  LOGINNAME         VARCHAR(128) not null,
+  PASSWORD          VARCHAR(128) not null,
+  ENABLED           CHAR(1) default 'Y',
+  LAST_UPDATE_DATE  DATETIME,
+  LAST_UPDATED_BY   VARCHAR(128),
+  CREATION_DATE     DATETIME,
+  CREATED_BY        VARCHAR(128),
+  PRIMARY KEY (ID)
+);
+
+
+
+
