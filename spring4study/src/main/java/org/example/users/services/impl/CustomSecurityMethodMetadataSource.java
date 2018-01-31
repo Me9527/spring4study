425f6970245c106c6d6f3588ced48ba24a5bc8a2
CPSDC <CPSDC@DESKTOP-7T1PPNB>
2018-01-31 15:13:08 +0800

    dd

---
+++ spring4study/src/main/java/org/example/users/services/impl/CustomSecurityMethodMetadataSource.java
@@ -0,0 +1,39 @@
+package org.example.users.services.impl;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.access.method.MapBasedMethodSecurityMetadataSource;
+
+
+public class CustomSecurityMethodMetadataSource extends MapBasedMethodSecurityMetadataSource {
+
+    protected final Log logger = LogFactory.getLog(getClass());
+
+//	private ICommonDAO commonDAO;
+    private String rolePrefix = "perm_";
+    private List<String> excludeUrls;
+    
+    public void afterPropertiesSet() throws java.lang.Exception{
+//		String hql = "from " + BResourcePermission.class.getSimpleName() ; 
+//		@SuppressWarnings("unchecked")
+//		List<BResourcePermission> rs = (List<BResourcePermission>) commonDAO.find(hql);
+//		if(null == rs)
+//			return;
+//		
+//		for(BResourcePermission perm: rs){
+//			if(perm.getRType() == 10){	//==10 表示url形式的资源权限,而不是业务层的(service层)
+//				SecurityConfig configAttribute = new SecurityConfig(rolePrefix+perm.getSeqId());
+//				Collection<ConfigAttribute> c = new HashSet<ConfigAttribute>();
+//				c.add(configAttribute);
+//				AntPathRequestMatcher requestMatcher = new AntPathRequestMatcher(perm.getResources());
+//				requestMap.put(requestMatcher, c);
+//			}
+//
+//		}
+    }
+    
+ 
+    
+}
